1730a96adb3cc06e870c9951d0fdf55d0de8f44d
adal.io <hello@adal.io>
2017-03-12 14:24:35 -0700

    Converted to simple find

--- sgitgc.sh
+++ sgitgc.sh
@@ -1,24 +1,3 @@
-# run git garbage collector in current directory
-
-gitgc() {
-  echo "git gc " $1;
-  git gc;
-}
-
-loop() {
-  echo "Starting sgitgc";
-  # for each directory in current directory excluding git
-  for D in `find . -mindepth 2 -maxdepth 2  -type d`
-  do
-      # Go to directory
-      cd $D;
-      # run gitgc function
-      gitgc $D;
-      # go back to parent directory
-      cd ../..;
-  done;
-  echo "sgitgc complete";
-}
-
-
-loop;
+#!/bin/bash
+MAX=${MAX:-5}
+find . -name '*.git' -mindepth 2 -maxdepth $MAX  -type d -execdir sh -c 'cd {} && echo Running in $(cd .. && pwd) && git gc' \;
